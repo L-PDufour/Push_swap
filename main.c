ab86573423cbebe88786f321d7ca7c750078c5d7
Leon-Pierre Dufour <ldufour@c1r4p2.42quebec.com>
2023-04-24 15:18:35 -0400

    depart

--- main.c
+++ main.c
@@ -3,24 +3,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void print_data(void *data) { printf("%s\n", (char *)data); }
+void	print_data(void *data)
+{
+	printf("%s\n", (char *)data);
+}
 
-int main(int argc, char **argv) {
+int	main(int argc, char **argv)
+{
+	int		i;
+	t_list	*a;
+	t_list	*new_node;
 
-  int i = 1;
+	i = 1;
+	a = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+	{
+		printf("1\n");
+		return (1);
+	}
+	while (argv[i] != NULL)
+	{
+		new_node = ft_lstnew(argv[i]);
+		ft_lstadd_front(&a, new_node);
+		i++;
+	}
+	ft_lstiter(a, print_data);
+	// argv = ft_split(argv[i], 32);
 
-  t_list *a = NULL;
-  if (argc == 1 || (argc == 2 && !argv[1][0])) {
-    printf("1\n");
-    return (1);
-  }
-  argv = ft_split(*argv, ' ');
-  while (argv[i] != NULL) {
-    t_list *new_node = ft_lstnew(*argv);
-    ft_lstadd_front(&a, new_node);
-    i++;
-  }
-  ft_lstiter(a, print_data);
+	// while (**argv)
+	// {
+	// 	printf("%s", argv[i]);
+	// 	i++;
+	// }
+	return (0);
 
-  return (0);
 }

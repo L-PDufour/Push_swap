5193c37e082e739c43f3d6f9e96a9d6ac8e19fcc
Leon-Pierre Dufour <ldufour@c1r4p2.42quebec.com>
2023-05-04 15:53:28 -0400

    testlibft

---
+++ Libft42/ft_lstnew.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldufour <marvin@42quebec.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 08:34:58 by ldufour           #+#    #+#             */
+/*   Updated: 2023/04/26 12:36:57 by ldufour          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstnew(int content) {
+  t_list *tmp;
+
+  tmp = NULL;
+  tmp = malloc(sizeof(t_list));
+  if (!tmp)
+    return (NULL);
+  tmp->content = content;
+  tmp->next = NULL;
+  return (tmp);
+  free(tmp);
+}

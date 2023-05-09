f01881d5471a521f00b3cea836f3b333d7f01e4e
Leon-Pierre Dufour <ldufour@c1r4p2.42quebec.com>
2023-05-09 15:28:51 -0400

    5algooo

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #! /bin/sh
 make all
-# ./push_swap 1 2 3 4 5 6
-# ./push_swap "100 50 20" 30 15 4 "3 2 1"
-# ./push_swap 2147483648 -2147483649
-./push_swap 6 5 4 3 2 1
+
+./push_swap 1 5 2 4 3 
+
+# ./push_swap 3 2 1
+#
+# ./push_swap 3 1 2
+#
+# ./push_swap 1 3 2
+#
+# ./push_swap 2 3 1
